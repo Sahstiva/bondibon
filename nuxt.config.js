f3eaf3f7c217c5e8fe01d4037266719afceae3a0
Sahstiva <avitshas@gmail.com>
2021-08-02 23:43:23 +0300

    screen 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,29 @@
 export default {
+    target: 'static',
+    router: {
+        base: '/bondibon/'
+    },
+    head: {
+        title: 'Nuxt',
+        titleTemplate: 'My amazing Nuxt application | %s',
+        htmlAttrs: {
+            lang: 'en',
+            amp: true
+        },
+        meta: [
+            {charset: 'utf-8'},
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content:
+                    'The amazing Nuxt application that teaches me all the cool features of Nuxt'
+            }
+        ],
+    },
     css: [
         '~/assets/style/variables.scss',
         '@fortawesome/fontawesome-svg-core/styles.css'
